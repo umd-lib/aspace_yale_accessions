9392de6ec9de1cddd4a30721c31bfd7efc5a066f
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2017-05-23 13:52:21 +0200

    LIBASPACE-33 LIBASPACE-34 Autogenerate accession and resource ids
    This modifies the plugin to autogenerate resource IDs and modifies the
    accession IDs.
    
    Resource IDs: id_0 is a sequence number, id_1 is selectbox with department
    codes.
    
    Accession IDs: id_0 is FY pulled from Accession year, id_1 is sequence number,
    id_2 is department code.
    
    https://issues.umd.edu/projects/LIBASPACE/issues/LIBASPACE-33
    https://issues.umd.edu/projects/LIBASPACE/issues/LIBASPACE-34

---
+++ backend/model/mixins/four_id_generator.rb
@@ -0,0 +1,59 @@
+require 'date'
+
+module FourIdGenerator
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+
+  def self.inside_import?
+    # Requests through the web UI will come in with a high priority, whereas
+    # migrations and batch imports will not.
+    !RequestContext.get(:is_high_priority)
+  end
+
+
+  @accession_fy_generator = lambda {|json|
+    date = Date.parse(json['accession_date'])
+    "#{date.month > 6 ? date.year + 1 : date.year}"
+  }
+
+
+
+
+  @sequence_generator = lambda {|json|
+
+    type = json["jsonmodel_type"]
+    sequence_name = "#{type}_#{json['id_0']}"
+
+    seq = Sequence.get(sequence_name)
+    seq = Sequence.get(sequence_name) if seq < 1
+
+    seq.to_s.rjust(4, '0')
+  }
+  
+  @import_generator = lambda {|json|
+    "import"
+  }
+
+
+  def self.accession_fy_generator
+    @accession_fy_generator
+  end
+
+
+  def self.sequence_generator
+    @sequence_generator
+  end
+
+
+  def self.import_generator
+    @import_generator
+  end
+
+
+  module ClassMethods
+
+  end
+end
