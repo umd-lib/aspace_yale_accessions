15fd82a68276ad43459073360720ca3039a63f57
David P. Steelman <dsteelma@umd.edu>
2026-02-04 11:40:10 -0500

    LIBASPACE-404. Fix placement of menu item in "Plug-ins" dropdown
    Modified the CSS selector in "repository_menu.js" to properly add
    the menu item under the "Plug-is" submenu, if it is present
    (it is present in Kubernetes, but not typically present in the
    local development environment).
    
    Cannot use the standard ArchivesSpace mechanism of specifying
    the menu item in a "repository_menu_controller" directive
    in a "config.yml" file for the plugin (see
    https://docs.archivesspace.org/customization/plugins/#plugin-configuration)
    because the associated endpoints are not the "index" action in
    the controller.
    
    Also updated the CSS in the HTML for ArchivesSpace 4.1.1.
    
    https://umd-dit.atlassian.net/browse/LIBASPACE-404

--- frontend/assets/repository_menu.js
+++ frontend/assets/repository_menu.js
@@ -1,9 +1,9 @@
 $(function() {
   var $target = $('li.repo-container .btn-group > ul.dropdown-menu');
 
-  if ($('li.repo-container .dropdown-submenu > a:contains("Plug-ins")').length > 0) {
-    $target = $('li.repo-container .dropdown-submenu > a:contains("Plug-ins")').parent().find('.dropdown-menu');
+  if ($('[data-toggle="dropdown"]:contains("Plug-ins")').length > 0) {
+    $target = $('[data-toggle="dropdown"]:contains("Plug-ins")').parent().find('.dropdown-menu');
   }
 
-  $target.append('<li class="dropdown-item"><a href="/plugins/yale_accessions/department_codes">Department Codes</a></li>');
+  $target.append('<li><a class="dropdown-item px-4 py-1" href="/plugins/yale_accessions/department_codes">Department Codes</a></li>');
 })
